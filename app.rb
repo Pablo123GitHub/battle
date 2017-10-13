1f71264b8b860f22f906cb05ab508d5d35445a9e
Pablo <pabloandresvdl@gmail.com>
2017-10-13 16:12:34 +0100

    change the player1, player2 glob variable with the game global variable

--- app.rb
+++ app.rb
@@ -10,25 +10,27 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player1 = Player.new(params[:player_1_name])
-    $player2 = Player.new(params[:player_2_name])
+    $game = Game.new(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
+    # $player1 = Player.new(params[:player_1_name])
+    # $player2 = Player.new(params[:player_2_name])
+
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player1.name
-    @player_2_name = $player2.name
-    @player_2_HP = $player2.hp # refactor in constant
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
+    @player_2_HP = $game.player2.hp # refactor in constant
     erb(:play)
   end
 
   post '/receive_attack' do
     p params
     'hello'
-    @player_1_name = $player1.name
-    @player_2_name = $player2.name
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
     # $player2.lose_life
-   Game.new.attack($player2)
+   $game.attack($game.player2)
 
     erb(:attack)
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,16 @@
 require 'game'
 describe Game do
-  let(:game) { Game.new}
+
   let(:player1) { double(:player1) }
   let(:player2) { double(:player2) }
+  let(:game) { Game.new(player1,player2)}
   it 'triggers the attack via the game class' do
     expect(player2).to receive(:lose_life)
     game.attack(player2)
+  end
+
+  it 'starts the game with two players' do
+     expect(Game).to receive(:new).with(player1, player2)
+     Game.new(player1,player2)
   end
 end
