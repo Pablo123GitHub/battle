53f201902deffa75b11f08b4e47d30946ca6bd6b
Pablo <pabloandresvdl@gmail.com>
2017-10-13 12:13:45 +0100

    added one line to feature Attacking test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,7 @@ feature 'Attacking' do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack!'
+    expect(page).not_to have_content 'Lucas HPs: 60'
     expect(page).to have_content 'Lucas points are now : 50'
   end
 end
